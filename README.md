636d724276d2d9ae1db874c8ab43d99a62072ede
soyqur <soyqureshi@gmail.com>
2024-06-26 19:42:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Soy
+# mywebsite
---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+function contactMe () {
+    document.getElementById ("sqheadname").innerHTML = "+91 9167-553-218";
+
+    document.querySelector ("#sqheadname").style.color = "chartreuse";
+    document.querySelector ("#sqheadname").style.animation = "sqfade 0.1s linear 25 alternate";
+    document.querySelector ("#sqheadname").style.fontSize = "0.9em";
+    document.querySelector (".head").style.animation = "fro 20s linear infinite alternate";
+}
+
+
+
+
+// function contactMe () { 
+//     document.getElementById ("sqheadname").innerHTML = ("55555555555");
+
+//     document.querySelector ("#sqheadname").style.backgroundColor = "chartreuse";
+// }
+
+
+function hireMeCv () {
+    alert ("Please email me on shoaib.qsh@gmail.com for CV");
+
+}
+
+
+function workInPogress () {
+    document.getElementById ("wip").innerHTML = "Site Under Construction";
+    
+}
